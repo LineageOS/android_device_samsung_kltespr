dd441cf5cb45a26c054773000e920d38dcea3fb1
Matt Filetto <matt.filetto@gmail.com>
2014-10-12 21:15:02 -0700

    kltespr: add "klte" to build barrier to fix flashing issues.
    * Some recoveries report the device as klte regardless of the variant,
    because of this the ROM fails to flash. This will at least allow
    flashing without editing the updater-script manually.
    
    Change-Id: Idd262080640bb3e2c3fa2e2c4d35d5c4504a9ccf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 # inherit from common klte
 -include device/samsung/klte-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := kltespr
+TARGET_OTA_ASSERT_DEVICE := kltespr,klte
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_spr_defconfig
