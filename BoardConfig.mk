80f6bb9f393ff6c113751ad9de3dcf92dc9177fa
ljzyal <ljzyal@gmail.com>
2015-05-25 23:02:33 +0800

    kltespr: Move mkbootimg to device (2/2)
    Change-Id: Ia7715fffededfe175d3c03eabddac5559fcc0646

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,7 @@
 TARGET_OTA_ASSERT_DEVICE := kltespr,klte
 
 # Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/kltespr/mkbootimg.mk
 TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_spr_defconfig
 
 # Init
