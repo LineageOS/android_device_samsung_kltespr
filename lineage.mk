2c8eef2ceab495cfb4bf5ae093fa9f5afd3c6f3e
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-29 22:03:01 -0700

    kltespr: Rebrand some of the things
    * Update PRODUCT_NAME because the value is user-visable
    * Rename cm.mk to lineage.mk because we are touching the file
    * Rename cm.dependencies to lineage.dependencies because why not
    
    Change-Id: I5c042e03b30e319bf20209a6a37278e85c9493be

--- lineage.mk
+++ lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
 
 PRODUCT_DEVICE := kltespr
-PRODUCT_NAME := cm_kltespr
+PRODUCT_NAME := lineage_kltespr
